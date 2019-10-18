c5bf0c32beed9527534ff32237cc50044b53b726
wtlin1228 <wtlin1228@gmail.com>
2019-10-18 16:11:32 +0800

    Updates

---
+++ precache-manifest.a5c6842d76bbad2d429ea032a947bb8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8578b2dafce28f531874ea4d293de2a3",
+    "url": "/react-palette/index.html"
+  },
+  {
+    "revision": "6fc4c7145bb9d891628b",
+    "url": "/react-palette/static/css/main.d3849cdf.chunk.css"
+  },
+  {
+    "revision": "277ac6fe474a9a39e7c4",
+    "url": "/react-palette/static/js/2.63e16ced.chunk.js"
+  },
+  {
+    "revision": "6fc4c7145bb9d891628b",
+    "url": "/react-palette/static/js/main.fde478a7.chunk.js"
+  },
+  {
+    "revision": "48f6cf1a6a24e4432992",
+    "url": "/react-palette/static/js/runtime-main.3a076bf5.js"
+  }
+]);
